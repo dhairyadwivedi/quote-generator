817dc64336242e82b2087d0ddce1da3ca2a7fc1f
Dhairya <dhairiya96@gmail.com>
2018-05-28 14:29:04 +0530

    Update quote-generator.js

--- assets/quote-generator.js
+++ assets/quote-generator.js
@@ -1,2 +1,5 @@
 $(document).ready(function() {
  	var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?&key="
+  var randomNum = Math.random() * 100000;
+  var randomWhl = Math.floor(randomNum);
+ 	var randomURL = url + randomWhl;
